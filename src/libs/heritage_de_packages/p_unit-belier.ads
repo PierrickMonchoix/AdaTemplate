dbb7076d0f00fceb5226fa15710228b99f0f777d
pierrick monchoix <pierrick.monchoix@gmail.com>
2021-10-18 12:15:07 +0200

    Ajout de commentaires pour la POO

--- src/libs/heritage_de_packages/p_unit-belier.ads
+++ src/libs/heritage_de_packages/p_unit-belier.ads
@@ -2,6 +2,9 @@ package P_Unit.Belier is
 
    type T_Belier is new T_Unit with private;
 
+   -- Sans overriding, la procedure Attaque de T_Belier aurati été:
+   --    procedure Attaque(Self : in out T_Fille ; Defenseur : in out T_Unit'class); de T_Unit (comment def de T_Unit.Attaque)
+   -- Là, par le ovveriding, on indique que la fonction Attaque est ré-écrite
    overriding procedure Attaque (Self : in out T_Belier ; Defenseur : in out T_Unit'class);
 
 
--- src/libs/heritage_de_packages/p_unit.ads
+++ src/libs/heritage_de_packages/p_unit.ads
@@ -4,13 +4,17 @@ package P_Unit is
 
    function Init_Unit (Att, Vie : Integer := 0) return T_Unit;
 
-   -- comme le premier param de Attaque est un T_Unit alors on poura faire Une_Unite.Attaque(Autre_Unite);
+   -- Comme le premier param de Attaque est un T_Unit alors on poura faire Une_Unite.Attaque(Autre_Unite);
+   -- Comme Self est de type T_Unit, une classe fille de T_Unit aura une fonction Attaque comme suit:
+   --    procedure Attaque(Self : in out T_Fille ; Defenseur : in out T_Unit'class);
+   -- Le 'class de "Defenseur : in out T_Unit'class" signifie que Defenseur acceptera tout type hérité de T_Unit
+   -- C'est bien fait car Ada permet de dire: Self ne poura être QUE du type de la classe en question
+   --   et Defenseur de n'import quel type dérivé. Imposer ce "QUE" n'est pas possible en Java par exemple.
    procedure Attaque
      (Self : in out T_Unit ;
       Defenseur : in out T_Unit'class);  -- 'class pour que TOUS les types herites de T_Unit fonctionnent
 
-   function Get_Vie(Self : T_Unit) return Integer; -- le "Self : T_Unit" deviendra automatiquement "Self : T_soingeur" pour la classe soigneur
-     -- comme pour la classe
+   function Get_Vie(Self : T_Unit) return Integer;
 
 private
 
